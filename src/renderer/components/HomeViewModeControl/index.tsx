dac30bcbee2989a7ce41815c948b17e5bb420cfe
ryhrm-gz <gyozaoisii@icloud.com>
2022-03-13 03:41:11 +0900

    update home view mode contorol

--- src/renderer/components/HomeViewModeControl/index.tsx
+++ src/renderer/components/HomeViewModeControl/index.tsx
@@ -1,12 +1,33 @@
-import { Box, Center, Group, SegmentedControl } from "@mantine/core";
-import { GridIcon, RowsIcon } from "@radix-ui/react-icons";
+import { Box, Button, Center, Group, SegmentedControl } from "@mantine/core";
+import {
+  ArrowDownIcon,
+  ArrowUpIcon,
+  GridIcon,
+  RowsIcon,
+} from "@radix-ui/react-icons";
+import { useState } from "react";
 
 export const HomeViewModeConstrol = () => {
+  const [updatedSort, setUpdatedSort] = useState("latest");
+  const updatedSortIcon =
+    updatedSort === "latest" ? <ArrowDownIcon /> : <ArrowUpIcon />;
   return (
     <Box sx={{ width: "100%" }} mb={5}>
-      <Group sx={{ width: "100%", height: "100%" }} position="right">
+      <Group sx={{ width: "100%", height: "100%" }} position="apart">
+        <Button
+          size="xs"
+          color="gray"
+          variant="subtle"
+          rightIcon={updatedSortIcon}
+          onClick={() =>
+            setUpdatedSort(updatedSort === "latest" ? "oldest" : "latest")
+          }
+        >
+          更新日
+        </Button>
         <Box>
           <SegmentedControl
+            size="sm"
             data={[
               {
                 value: "grid",
