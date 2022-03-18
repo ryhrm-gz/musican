cdfb9357b1e3381b2f7575f492c541ded1fba250
ryhrm-gz <gyozaoisii@icloud.com>
2022-03-19 02:29:50 +0900

    add updated sort

--- src/renderer/components/HomeList/index.tsx
+++ src/renderer/components/HomeList/index.tsx
@@ -2,12 +2,20 @@ import { Grid, ScrollArea } from "@mantine/core";
 import { useLiveQuery } from "dexie-react-hooks";
 import { useAtomValue } from "jotai";
 import { db } from "../../db";
+import { updatedSortState } from "../../state/updatedSortState";
 import { viewModeState } from "../../state/viewModeState";
 import { HomeCard } from "./HomeCard";
 
 export const HomeList = () => {
+  const updatedSort = useAtomValue(updatedSortState);
   const viewMode = useAtomValue(viewModeState);
-  const projects = useLiveQuery(() => db.projects.reverse().toArray());
+  const projects = useLiveQuery(() => {
+    if (updatedSort === "latest") {
+      return db.projects.where("id").above(0).reverse().sortBy("updatedAt");
+    } else {
+      return db.projects.where("id").above(0).sortBy("updatedAt");
+    }
+  }, [updatedSort]);
   return (
     <ScrollArea style={{ height: "calc(100vh - 150px)" }} scrollbarSize={8}>
       {viewMode === "grid" ? (
